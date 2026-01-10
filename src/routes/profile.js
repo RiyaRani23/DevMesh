904d9e26a049f3b9a60f80395b5cf25da83bc941
RiyaRani23 <riyarani1404@gmail.com>
2026-01-10 23:36:20 +0530

    Added profile Edit feature

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const { validateEditProfiledata } = require("../utils/validation");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
@@ -10,6 +11,26 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
   catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
+});
+
+// Edit Profile Route - to update logged-in user's profile
+profileRouter.patch("/profile/edit", userAuth, async (req, res) =>{
+    try {
+        if(!validateEditProfiledata(req)){
+            throw new Error("Invalid edit request!");
+        }
+        const loggedInUser = req.user;
+        Object.keys(req.body).forEach((key) => {
+            loggedInUser[key] = req.body[key];
+        });
+        await loggedInUser.save();
+        res.json({
+            message:`${loggedInUser.firstName}, Your profile updated successfully!`,
+            user: loggedInUser,
+        });
+    }catch(err){
+        res.status(400).send("ERROR : " + err.message);
+    }
 });
 
 module.exports = profileRouter
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,7 @@
 const validator = require('validator');
 
 const validateSignUpData = (req) => {
-    const {firstName, lastName, emailId, password} = req.body;
+    const {firstName, lastName, emailId, password, gender} = req.body;
 
     if(!firstName || typeof firstName !== 'string' || !lastName || typeof lastName !== 'string') {
         throw new Error("First name and Last name are required and must be strings");
@@ -10,7 +10,19 @@ const validateSignUpData = (req) => {
     } else if(!password || !validator.isStrongPassword(password)) {
         throw new Error("A strong password is required");
     }
+    else if (gender < 17) {
+    throw new Error("Age must be at least 18 years!");
+    }
 };
+
+// Function to validate edit profile data
+const validateEditProfiledata = (req) => {
+  const allowedFields = ["firstName", "lastName", "photoUrl", "password", "about", "skills", "age", "gender"];
+
+  const isEditAllowed =  Object.keys(req.body).every(field => allowedFields.includes(field));
+  return isEditAllowed;
+}
 module.exports = {
     validateSignUpData,
+    validateEditProfiledata,
 };
