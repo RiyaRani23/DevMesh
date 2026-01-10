3ee6bc2a81668b713a764643144e40656dff6844
RiyaRani23 <riyarani1404@gmail.com>
2026-01-10 21:05:09 +0530

    Middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,24 +1,34 @@
 // Middleware is a function that runs before the actual request handler 
-const adminAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked!!");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if (!isAdminAuthorized) {
-        return res.status(403).send("Admin not authorized");
-    }  else {
-        next();
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
+
+// Middleware to authenticate user using JWT token
+const userAuth = async (req, res, next) => {
+  try {
+    // Read the token from cookies
+    const { token } = req.cookies;
+    if (!token) {
+      return res.status(401).send("Please Login!");
     }
+    const decodedObj = await jwt.verify(token, process.env.JWT_SECRET); // verify token
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+
+    req.user = user;
+    next(); // to move to the req handler
+  }
+  catch (err) {
+    res.status(401).send("Unauthorized : " + err.message);
+  }
 };
 
-const userAuth = (req, res, next) => {
-    console.log("Admin auth is getting checked!!");
-    const token = "xyz";
-    const isAdminAuthorized = token === "xyz";
-    if (!isAdminAuthorized) {
-        return res.status(403).send("Admin not authorized");
-    }  else {
-        next();
-    }
+module.exports = {
+  userAuth,
 };
-
-module.exports = { adminAuth, userAuth };
