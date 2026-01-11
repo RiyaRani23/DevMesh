11392380d137325faae8dad6100533e40e73832a
RiyaRani23 <riyarani1404@gmail.com>
2026-01-11 13:21:15 +0530

    userRouter all connection route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -24,7 +24,34 @@ userRouter.get("/user.requests/recieved", userAuth, async (req, res) => {
   } catch (err) {
          req.statusCode(400).send("Error" + err.message);
     }
-   const loggedInUser = req.user;
+});
+
+// Get all the connections for the loggedIn user
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // Define safe user data fields to be returned
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
 });
 
 module.exports = userRouter;
